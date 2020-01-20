bc755f029af979596beefaf2c55b666e0526e437
TymNim <tymnim@gmail.com>
2020-01-19 21:32:39 -0500

    Initial Commit

---
+++ Snake/GameScene.swift
@@ -0,0 +1,22 @@
+//
+//  GameScene.swift
+//  Snake
+//
+//  Created by Tim Nimets on 2020-01-19.
+//  Copyright © 2020 Tim Nimets. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
